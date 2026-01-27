4f1d80fc95e026c5fb7c0ed1c62326aa75af770a
Kalana-indula <kalanasmedia@outlook.com>
2026-01-27 22:34:27 +0530

    appside bar added

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import AppSidebar from "@/components/AppSidebar";
 
 const ProtectedLayout = ({children}:{children:React.ReactNode}) => {
     return (
-        <div>
-            <div>
+        <div className="w-full h-screen flex bg-gray-200">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
             {/*  Sidebar  */}
+                <AppSidebar/>
             </div>
-            <div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] flex flex-col">
             {/*  Navbar  */}
-                <div>{children}</div>
+                <div className="h-full w-full p-2 overflow-y-scroll">{children}</div>
             </div>
         </div>
     )
