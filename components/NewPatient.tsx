f67f84f009a6971f974f0b5840503984a492f002
Kalana-indula <kalanasmedia@outlook.com>
2026-01-28 15:05:00 +0530

    schemas added

--- components/NewPatient.tsx
+++ components/NewPatient.tsx
@@ -3,6 +3,12 @@
 import type { Patient } from "@/generated/prisma";
 import React, {useState} from 'react'
 import {useUser} from "@clerk/nextjs";
+import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {useRouter} from "next/navigation";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
+import {z} from "zod";
+import {PatientFormSchema} from "@/lib/schema";
 
 interface DataProps {
     data?:Patient;
@@ -11,11 +17,35 @@ interface DataProps {
 
 const NewPatient = ({data,type}:DataProps) => {
 
-    const user = useUser();
+    const {user} = useUser();
     const [loading,setLoading]=useState(false);
+    const [imgURL,setImgURL]=useState<any>();
+    const router=useRouter()
+
+    const userData = {
+        first_name:user?.firstName || "",
+        last_name:user?.lastName || "",
+        email:user?.emailAddresses[0].emailAddress || "",
+        phone:user?.phoneNumbers?.toString() || "",
+    };
+
+
+    const form = useForm<z.infer<typeof PatientFormSchema>>({
+        resolver:zodResolver(PatientFormSchema),
+        defaultValue:{
+            ...userData
+        },
+    })
 
     return (
-        <div>NewPatient</div>
+        <Card className="max-w-6xl w-full p-4">
+            <CardHeader>
+                <CardTitle>Patient Registration</CardTitle>
+                <CardDescription>
+                    Please provide all the information below to help us understand better and provide good and quality service to you
+                </CardDescription>
+            </CardHeader>
+        </Card>
     )
 }
 export default NewPatient
